8bfd1938dc300b322cd1fc43f253b4ae87f452c4
aliceszzi <73361814+aliceszzi@users.noreply.github.com>
2021-12-07 01:20:14 -0300

    Add files via upload

---
+++ herança2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace herança2
+{
+    public class cPBase
+    {
+        private string nome;
+        private string cidade;
+        private int nFilhos;
+        private int idade;
+        private int Uf;
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+        public int NFilhos { get => nFilhos; set => nFilhos = value; }
+        public int Idade { get => idade; set => idade = value; }
+        public int Uf1 { get => Uf; set => Uf = value; }
+    }
+}
+
+namespace herança2
+{
+
+    public class cPFisica : cPBase
+    {
+
+        private string cpf;
+        private string mAniversario;
+        public string Cpf { get => cpf; set => cpf = value; }
+        public string MAniversario { get => mAniversario; set => mAniversario = value; }
+    }
+
+}
+
+namespace herança2
+{
+
+    public class cPJuridica : cPBase
+    {
+
+        private string cnpj;
+        private string rsocial;
+        private string nfuncionarios;
+
+        public string Cnpj { get => cnpj; set => cnpj = value; }
+        public string Rsocial { get => rsocial; set => rsocial = value; }
+        public string Nfuncionarios { get => nfuncionarios; set => nfuncionarios = value; }
+    }
+
+}
